91862ee064ebc9fe8a8de9647adb3aa81fc84d43
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2023-09-14 03:48:00 -0300

    header home

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
+import { Github } from "lucide-react";
 import { Button } from "./components/ui/button";
+import { Separator } from "./components/ui/separator";
 
 export function App() {
 
   return (
-   <>
-    <h1>Home</h1>
-    <Button> Click Me! </Button>
-   </>
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground" >
+            Funcionalidades com auxilio de AI
+          </span>
+
+          <Separator orientation="vertical"  className="h-6"/>
+
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" />
+            Github
+          </Button>
+        </div>
+      </div>
+    </div>
   )
 }
 
